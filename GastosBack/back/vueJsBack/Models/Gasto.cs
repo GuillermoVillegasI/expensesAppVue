95f05a18c55560b527c8fd02b54b500094164a9a
Guillermo Villegas <guillermo.villegas@beclevercorp.com>
2023-03-27 17:54:25 -0300

    agregando inicio Back

---
+++ GastosBack/back/vueJsBack/Models/Gasto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vueJsBack.Models
+{
+    public class Gasto
+    {
+
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string name { get; set; }
+        [Required]
+        public double value { get; set; }
+    }
+}
